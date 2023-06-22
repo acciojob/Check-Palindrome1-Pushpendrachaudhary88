6e1ba32786a202841aa5927c03126dbc6e78833c
Pushpendra Chaudhary <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-06-22 12:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	// function isPalindrome(s) {
+  // Remove non-alphanumeric characters
+  str = str.replace(/[^a-zA-Z0-9]/g, '');
 
+  // Convert to lowercase
+  str = str.toLowerCase();
+
+  // Check if the string is equal to its reverse
+  return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
